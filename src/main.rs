b734a839b088b5ce89278746a83e9fb5ac039cad
James Hart <james.elliot.hart@gmail.com>
2024-09-30 13:33:53 +0000

    implement CompletedItem

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+
+mod done {
+    use core::fmt;
+
+    use chrono::{DateTime, Duration, Utc};
+
+    pub struct CompletedItem {
+        pub completed: DateTime<Utc>,
+        pub item: String,
+    }
+
+    impl fmt::Display for CompletedItem {
+        fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+            write!(f, "[{}] {}", self.completed, self.item)
+        }
+    }
+
+    impl CompletedItem {
+        pub fn new(item: String, completed_on: Option<DateTime<Utc>>) -> Self {
+            let completed = completed_on.unwrap_or(Utc::now());
+            Self { completed, item }
+        }
+
+        pub fn now(item: String) -> Self {
+            Self::new(item, None)
+        }
+    }
+
+}
+
+
 fn main() {
     println!("Hello, world!");
 }
